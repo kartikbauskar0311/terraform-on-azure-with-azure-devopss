ee43b7ea664a841f5e0eb5abf4bde6990560eb58
kartik bauskar <kartik93.bauskar@gmail.com>
2024-11-20 10:06:54 -0500

    First commit

---
+++ tm/c1-versions.tf
@@ -0,0 +1,31 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0"
+    }
+  }
+}
+
+# Terraform State Storage to Azure Storage Container (Values will be taken from Azure DevOps)
+/*
+backend "azurerm" {
+  
+}  */
+# Provider Block
+provider "azurerm" {
+  features {}
+  subscription_id = "599ea008-5630-47a0-be71-a4858903a2b1"
+}
+
+
